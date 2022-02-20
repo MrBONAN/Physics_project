d7ba6f88cad002ed560c661a4b1cc1745b4dc771
НоутД <taleshatrycob@gmail.com>
2022-02-20 12:54:38 +0500

    Черновик InfoWinfow

---
+++ Physics_project/InfoWindow.cpp
@@ -0,0 +1,78 @@
+#include "InfoWindow.h"
+
+string pad(std::string s, int len = 26);
+
+ConfirmWindow::ConfirmWindow(string text) :
+	window(sf::VideoMode(530, 155), L"Проект"),
+	info(window, { INFOsize, 260, 50 }),
+	ok(window, { BUTTONsize, 32, 20 }) // 40 20
+{
+	window.setFramerateLimit(30);
+
+	info.setScale(2);
+	info.setPosition(5, 5);
+	info.setStr(text);
+
+	ok.setScale(2);
+	ok.setPosition(241, 110);
+	ok.setStr(pad("ОК", 4));
+}
+
+string ConfirmWindow::loop()
+{
+	while (window.isOpen() && result == "NONE")
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed) {
+				result = "CLOSE";
+				window.close();
+				continue;
+			}
+			checkAllInteractions(event);
+		}
+		show();
+	}
+	return result;
+}
+
+void ConfirmWindow::checkAllInteractions(const sf::Event& event)
+{
+	//кнопка только что нажата
+	if (event.type == sf::Event::MouseButtonPressed) {
+		checkAllActive(sf::Mouse::getPosition(window));
+		checkAllFocus(sf::Mouse::getPosition(window), true);
+	}
+	//кнока нажата в данный момент
+	if (sf::Mouse::isButtonPressed(sf::Mouse::Button::Left)) {
+		checkAllFocus(sf::Mouse::getPosition(window));
+	}
+	//кнопка была отпущена
+	if (event.type == sf::Event::MouseButtonReleased) {
+		checkAllEvents(sf::Mouse::getPosition(window));
+	}
+}
+
+void ConfirmWindow::checkAllActive(const sf::Vector2i& msCord)
+{
+	ok.checkActive(msCord);
+}
+
+void ConfirmWindow::checkAllFocus(const sf::Vector2i& msCord, bool first)
+{
+	ok.checkFocus(msCord);
+}
+
+void ConfirmWindow::checkAllEvents(const sf::Vector2i& msCord)
+{
+	if (ok.Event(msCord)) result = "OK";
+}
+
+void ConfirmWindow::show()
+{
+	window.clear(sf::Color(200, 200, 200, 255));
+	info.show();
+	ok.show();
+	window.display();
+}
---
+++ Physics_project/InfoWindow.h
@@ -0,0 +1,24 @@
+#pragma once
+#include "Button.h"
+
+class ConfirmWindow
+{
+public:
+	ConfirmWindow(string text);
+
+	string loop();
+
+private:
+	void checkAllInteractions(const sf::Event&);
+	void checkAllActive(const sf::Vector2i& msCord);
+	void checkAllFocus(const sf::Vector2i& msCord, bool first = false);
+	void checkAllEvents(const sf::Vector2i& msCord);
+
+	void show();
+
+	sf::RenderWindow window;
+	Button info, ok;
+
+	string result = "NONE";
+};
+
